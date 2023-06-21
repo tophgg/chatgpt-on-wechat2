c7edeac8822300821d6e20a52f0f0160aeb3c480
jackydong <xinghaodong@rastar.com>
2023-06-21 09:33:45 +0800

    try hot reload

--- channel/wechat/wechat_channel.py
+++ channel/wechat/wechat_channel.py
@@ -136,23 +136,21 @@ class WechatChannel(ChatChannel):
             hotReload=True,
             statusStorageDir=status_path,
             qrCallback=qrCallback,
-            loginCallback=self.login_callback,
-            exitCallback=self.logout_callback
+            # loginCallback=self.login_callback,
+            # exitCallback=self.logout_callback
         )
         print('statusStorageDir:', status_path)
         self.user_id = itchat.instance.storageClass.userName
         self.name = itchat.instance.storageClass.nickName
         logger.info("Wechat login success, user_id: {}, nickname: {}".format(self.user_id, self.name))
         # start message listener
-        itchat.run()
-        
-        print('startup running')
         WechatChannel().send(Reply(type=ReplyType.TEXT, content="我来了"), Context(kwargs={"receiver": "rpdhao"}))
         def dd():
             now = datetime.datetime.now()
             ts = now.strftime('%Y-%m-%d %H:%M:%S')
             WechatChannel().send(Reply(type=ReplyType.TEXT, content="整点报时,现在是"+ts), Context(kwargs={"receiver": "rpdhao"}))
         _start_schedule_deamon(dd)
+        itchat.run()
 
     # handle_* 系列函数处理收到的消息后构造Context，然后传入produce函数中处理Context和发送回复
     # Context包含了消息的所有信息，包括以下属性
